c9fdad7dce5bb8ce3cbdb4faa018761d240e3965
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-21 21:00:37 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ScopeAndSwitchSalvador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
